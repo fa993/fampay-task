a2a06391af33696dfa74b7906caa8795a1ba0a1f
ameyakore <kore.ameya@gmail.com>
2023-08-18 17:41:12 +0530

    feat: finished dashboard and bug fixes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, Response, json, request, render_template
 from pymongo import MongoClient
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 import pymongo
 from dotenv import load_dotenv
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -12,12 +12,12 @@ load_dotenv()
 
 client = MongoClient(host=os.environ["DB_URI"])
 
-db = client.get_default_database()
+db = client.get_default_database(default="ytsc")
 vids = db[poptask.src_term+"vids"]
 
 sched = BackgroundScheduler(daemon=True)
-# sched.add_job(poptask.exec_sche, "interval", minutes=10)
-# sched.add_job(poptask.exec_sche_urg, 'interval', seconds=10)
+sched.add_job(poptask.exec_sche, "interval", minutes=10)
+sched.add_job(poptask.exec_sche_urg, 'interval', seconds=10)
 sched.start()
 
 app = Flask(__name__)
@@ -25,15 +25,21 @@ app = Flask(__name__)
 
 @app.route("/")
 def dashboard():
-    # poptask.act = 6
-    out = list(vids.find({}, {"_id": 0}).sort(
+    poptask.act_lock.acquire()
+    poptask.act = 6
+    poptask.act_lock.release()
+    ts = datetime.utcnow()
+    ts = (ts - timedelta(minutes=1))
+    out = list(vids.find({"publishedAt": {"$lt": ts}}, {"_id": 0}).sort(
         'publishedAt', pymongo.DESCENDING).limit(10))
     return render_template("index.html", out=out)
 
 
 @app.route("/querydata")
 def qdata():
+    poptask.act_lock.acquire()
     poptask.act = 6
+    poptask.act_lock.release()
     length = request.args.get("length", default=10)
     out = []
     if request.args.get("last") != None:
@@ -59,10 +65,15 @@ def qdata():
 
 @app.route("/changesearch", methods=['POST'])
 def changesearchterm():
-    if len(str(request.data)) >= 2:
-        poptask.src_term = request.data
+    global vids
+    dts = str(request.data)[2:-1]
+    if len(dts) >= 2:
+        poptask.src_term = dts
+        vids = db[poptask.src_term+"vids"]
         poptask.create_collection()
+        poptask.act_lock.acquire()
         poptask.act = 6
+        poptask.act_lock.release()
         return Response(status=200)
     else:
         return Response(status=400)
--- poptask.py
+++ poptask.py
@@ -6,15 +6,17 @@ import requests
 import os
 import json
 import logging
+import threading
 
 load_dotenv()
 
 src_term = os.environ["YT_QUERY"]
 
 client = MongoClient(host=os.environ["DB_URI"])
-db = client.get_default_database()
+db = client.get_default_database(default="ytsc")
+vids = db[src_term+"vids"]
 
-keys = map(lambda x: x.strip(), os.environ["API_KEY"].split(","))
+keys = list(map(lambda x: x.strip(), os.environ["API_KEY"].split(",")))
 counter = 0
 
 
@@ -43,6 +45,7 @@ create_collection()
 doing = 0
 
 act = 1
+act_lock = threading.Lock()
 
 
 def exec_sche():
@@ -50,7 +53,7 @@ def exec_sche():
     global counter
     doing = 1
     ts = (datetime.now(timezone.utc) -
-          timedelta(hours=1)).astimezone().isoformat()
+          timedelta(hours=10)).astimezone().isoformat()
 
     payload = {"part": "snippet", "maxResults": 25,
                "q": src_term, "key": keys[counter],
@@ -67,10 +70,8 @@ def exec_sche():
     try:
         vids.insert_many(rtp, ordered=False)
     except Exception as e:
-        print(e)
         # do nothing
         pass
-    print("Finished exec")
     logging.info("Scheduled Insert Finished")
     counter = (counter + 1) % len(keys)
     doing = 0
@@ -79,6 +80,8 @@ def exec_sche():
 def exec_sche_urg():
     global act
     if act > 0:
+        act_lock.acquire()
         act = act - 1
+        act_lock.release()
         if doing == 0:
             exec_sche()
